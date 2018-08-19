60e49ab91dc568a8ccc02a44ec090948c140d391
TsJazz27Sumin <ts.jazz.27.sumin@gmail.com>
2018-08-19 22:52:21 +0900

    検索条件部分の追加

--- SampleCoreWebApplication/ClientApp/components/UserMaintenance.tsx
+++ SampleCoreWebApplication/ClientApp/components/UserMaintenance.tsx
@@ -4,17 +4,42 @@ import { RouteComponentProps } from 'react-router';
 interface UserMaintenanceState {
     users: User[];
     loading: boolean;
+    textValue: string;
 }
 
 export class UserMaintenance extends React.Component<RouteComponentProps<{}>, UserMaintenanceState> {
     constructor() {
         super();
-        this.state = { users: [], loading: true };
+        this.state = { users: [], loading: true, textValue: "A" };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
 
         fetch('api/UserMaintenance/Users')
             .then(response => response.json() as Promise<User[]>)
             .then(data => {
                 this.setState({ users: data, loading: false });
+            });
+    }
+
+    handleInputChange(event: { target: any; }) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event: { preventDefault: () => void; }) {
+
+        event.preventDefault();
+        
+        fetch('api/UserMaintenance/Search?name=' + this.state.textValue)
+            .then(response => response.json() as Promise<User[]>)
+            .then(data => {
+                this.setState({ users: data, loading: false, textValue: this.state.textValue });
             });
     }
 
@@ -26,6 +51,13 @@ export class UserMaintenance extends React.Component<RouteComponentProps<{}>, Us
         return <div>
             <h1>UserMaintenance</h1>
             <p>This component demonstrates fetching data from the server.</p>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name="textValue" value={this.state.textValue} onChange={this.handleInputChange} />
+                </label>
+                <button>Search</button>
+            </form>
             {contents}
         </div>;
     }
--- SampleCoreWebApplication/Controllers/UserMaintenanceController.cs
+++ SampleCoreWebApplication/Controllers/UserMaintenanceController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SampleCoreWebApplication.Models;
 
@@ -22,8 +23,41 @@ namespace SampleCoreWebApplication.Controllers
                     Name = "B",
                     Occupation = "Teachar",
                     Age = "31"
+                },
+                new User() {
+                    Id = "U003",
+                    Name = "C",
+                    Occupation = "President",
+                    Age = "45"
                 }
             };
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<User> Search(string name)
+        {
+            var list = new List<User>(){
+                new User() {
+                    Id = "U001",
+                    Name = "A",
+                    Occupation = "Engineer",
+                    Age = "20"
+                },
+                new User() {
+                    Id = "U002",
+                    Name = "B",
+                    Occupation = "Teachar",
+                    Age = "31"
+                },
+                new User() {
+                    Id = "U003",
+                    Name = "C",
+                    Occupation = "President",
+                    Age = "45"
+                }
+            };
+
+            return string.IsNullOrEmpty(name) ? list :list.Where(user => user.Name == name);
         }
     }
 }
